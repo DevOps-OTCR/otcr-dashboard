3d13ded28bc2f55908655a57eb3b0bd2f01f085a
MeghP89 <meghp283@gmail.com>
2026-02-23 15:09:30 -0600

    fixed cors on backend for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://otcr-dashboard-qa.vercel.app'
+  ];
+
   // Enable CORS for frontend
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
   });
 
